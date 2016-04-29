0aa959775358a96dcfe467e807a7ed39b9c058fc
Brandon Frazier <bfrazier1018@gmail.com>
2016-04-29 11:53:59 -0400

    Add Play Audio Preview and On Click Button

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ $(document).on("ready", function() {
 		findTracks();
 	});
 
+	$(".btn-play").on("click", function() {
+		$(".play-preview").trigger("play");
+		$(".play-preview").
+	});
+
 });
 
 // Find and Display Tracks ---------------------------------------
@@ -15,6 +20,7 @@ function findTracks() {
 		success: function(data) {
 			// console.log(data);
 			displayTracks(data.tracks.items[0]);
+			$(".js-search-form").trigger("reset");
 		},
 		error: function(error) {
 			console.log("ERROR");
@@ -27,4 +33,10 @@ function displayTracks(track) {
 	$(".title").append(track.name);
 	$(".author").append(track.artists[0].name);
 	$(".cover").append(`<img src="${track.album.images[0].url}">`);
+	var audioUrl = track.preview_url;
+	playAudio(audioUrl);
+};
+
+function playAudio(url) {
+	$(".js-audio-player").append(`<audio src="${url}" class="play-preview"></audio>`);
 };
